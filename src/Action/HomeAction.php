e2cdb3dea00ebdbd8c652fd37f2992d78a4e3a1e
McFog Wang <mcfogw@gmail.com>
2019-04-08 21:55:38 +0800

    💄 style our default HTMLs

--- src/Action/HomeAction.php
+++ src/Action/HomeAction.php
@@ -11,9 +11,17 @@ use Zend\Diactoros\Response\HtmlResponse;
 class HomeAction extends BaseAction
 {
     const SOURCE = <<<'HTML'
-<html lang="en"><body>
-<h1>Hello LitPHP!</h1>
-</body></html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.css'>
+</head>
+<body>
+<h1>Hello <strong>LitPHP</strong></h1>
+<hr>
+There's <a href="./404">a not found page</a>.
+</body>
+</html>
 HTML;
 
     protected function main(): ResponseInterface
--- src/Action/NotFoundAction.php
+++ src/Action/NotFoundAction.php
@@ -12,13 +12,17 @@ class NotFoundAction extends BaseAction
 {
     const SOURCE = <<<'HTML'
 <html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.css'>
+</head>
 <body>
-<h1>404 Not  Found</h1>
+<h1>404 Not Found</h1>
 <hr>
-<h3>How to create a new page</h3>
+<h3>How to create a new page <sup style="opacity: .7"><em>Bonus!</em></sup></h3>
 <ol>
-<li>create an action class <code>NewProject\Action\YourAction</code></li>
-<li>register it to router in <code>NewProject\RouteDefinition</code></li>
+    <li>create an action class <code>NewProject\Action\YourAction</code></li>
+    <li>register it to router in <code>NewProject\RouteDefinition</code></li>
 </ol>
 </body>
 </html>
