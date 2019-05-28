6559dca9896925e13e3904619b4392d87b0e20c4
McFog Wang <mcfogw@gmail.com>
2019-05-28 06:03:35 +0200

    ✨ show more information

--- src/Action/HomeAction.php
+++ src/Action/HomeAction.php
@@ -13,13 +13,14 @@ class HomeAction extends BaseAction
     const SOURCE = <<<'HTML'
 <html lang="en">
 <head>
+<title>Welcome</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.css'>
 </head>
 <body>
 <h1>Hello <strong>LitPHP</strong></h1>
 <hr>
-There's <a href="./404">a not found page</a>.
+There's <a href="./404">a not found page</a> with more information.
 </body>
 </html>
 HTML;
--- src/Action/NotFoundAction.php
+++ src/Action/NotFoundAction.php
@@ -13,13 +13,13 @@ class NotFoundAction extends BaseAction
     const SOURCE = <<<'HTML'
 <html lang="en">
 <head>
+    <title>Not Found</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.css'>
 </head>
 <body>
 <h1>
     404 Not Found
-    <small style="color: silver">%s</small>
 </h1>
 <hr>
 <h3>How to create a new page <sup style="opacity: .7"><em>Bonus!</em></sup></h3>
@@ -27,14 +27,32 @@ class NotFoundAction extends BaseAction
     <li>create an action class <code>NewProject\Action\YourAction</code></li>
     <li>register it to router in <code>NewProject\RouteDefinition</code></li>
 </ol>
+<fieldset>
+    <legend>Request Information</legend>
+    <dl>
+        <dt>Method</dt><dd>METHOD</dd>
+        <dt>Host</dt><dd>HOST</dd>
+        <dt>Path</dt><dd>PATH</dd>
+        <dt>User-Agent</dt><dd>UA</dd>
+    </dl>
+</fieldset>
 </body>
 </html>
 HTML;
 
     protected function main(): ResponseInterface
     {
-        $path = $this->request->getUri()->getPath();
-        
-        return new HtmlResponse(sprintf(self::SOURCE, $path), 404);
+        $uri = $this->request->getUri();
+        $path = $uri->getPath();
+        $host = $this->request->getHeaderLine('Host');
+        $method = $this->request->getMethod();
+
+        $ua = $this->request->getHeaderLine('User-Agent');
+
+        // a dirty string templating with `strtr`, only for demo
+        $values = array_change_key_case(get_defined_vars(), CASE_UPPER);
+        $html = strtr(self::SOURCE, $values);
+
+        return new HtmlResponse($html, 404);
     }
 }
